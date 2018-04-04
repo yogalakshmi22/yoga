466b4dd486ff165b9edfbc04340a7260884908c8
yogalakshmi22 <yoga230204@gmail.com>
2018-04-04 12:43:27 +0530

    json

--- main.js
+++ main.js
@@ -1,38 +1,78 @@
-function loadJson(file,callback){
-  var xhr=new XMLHttpRequest();
-  xhr.overrideMimeType("application/json");
-  xhr.open("GET",file,true);
-  xhr.onreadystatechange=function(){
-    if(xhr.readyState ===4 && xhr.status== "200"){
-      callback(xhr.responseText);
-    }
-  };
-  xhr.send();
+function loadJSON(file,callback)
+{
+ var xhr=new XMLHttpRequest();
+ xhr.overrideMimeType("application/json");
+ xhr.open("GET",file,true);
+ xhr.onreadystatechange=function()
+ {
+   if(xhr.readyState ===4 && xhr.status =="200")
+   {
+     callback(xhr.responseText);
+   }
+ };
+ xhr.send();
 }
-loadJson("data.json",function(text){
-  let data=JSON.parse(text);
-  console.log(data);
-  career(data.career);
-  education(data.education);
+loadJSON("data.json",function(text)
+{
+ let data=JSON.parse(text);
+ console.log(data);
+ career(data.career);
+ education(data.education);
+ skills(data.skills);
 })
 var right=document.querySelector(".content");
 function career(car){
-  console.log(car.info);
+ console.log(car.info);
+ var h3=document.createElement("h3");
+ h3.textContent="Career Objective";
+ right.appendChild(h3);
+   var hr=document.createElement("hr");
+   right.appendChild(hr);
+   var p=document.createElement("p");
+   p.textContent=car.info;
+   right.appendChild(p);
+}
+function education(edu){
+ var h3=document.createElement("h3");
+ h3.textContent="Education";
+ right.appendChild(h3);
+ var hr=document.createElement("hr");
+ right.appendChild(hr);
+ var ul=document.createElement("ul");
+   right.appendChild(ul);
+   for(var i=edu.length-1;i>=0;i--)
+   {
+     console.log(edu[i].degree);
+     var li=document.createElement("li");
+     li.textContent=edu[i].degree;
+     ul.appendChild(li);
+     var p=document.createElement("p");
+     p.textContent=edu[i].school;
+     li.appendChild(p);
+     var ul1=document.createElement("ul");
+     ul.appendChild(ul1);
+     for(j=0;j<edu[i].data.length;j++)
+     {
+       var li1=document.createElement("li");
+       li1.textContent=edu[i].data[j];
+       ul1.appendChild(li1);
+     }
+   }
+}
+function skills(skill)
+{
   var h3=document.createElement("h3");
-  h3.textContent="career Objective";
+  h3.textContent="skills";
   right.appendChild(h3);
   var hr=document.createElement("hr");
   right.appendChild(hr);
-  var p=document.createElement("p");
-  p.textContent=car.info;
-  right.appendChild(p);
-}
-function education(edu)
-{
- var h3=document.createElement("h3");
- h3.textContent="EDUCATION";
- right.appendChild(h3);
- var hr=document.createElement("hr");
- h3.textContent="EDUCATION";
- right.appendChild(hr);
+  var table=document.createElement("table");
+  table.border="1";
+  right.appendChild(table);
+  let row="";
+  for(var i=0;i<skill.length;i++)
+  {
+    row+="<tr><td>"+skill[i].name+"</td><td>"+skill[i].info+"</td></tr>";
+  }
+  table.innerHTML=row;
 }
